9c8048619e59b3f3c7fc60b3322175378b1f953e
Timiemmy <faith.bolanle@yahoo.com>
2023-03-10 11:49:50 +0100

    Updated the authentication to create user for social user auth

--- account/authentication.py
+++ account/authentication.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from account.models import Profile
 
 
 class EmailAuthBackend:
@@ -23,3 +24,9 @@ class EmailAuthBackend:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
+
+
+# This function creates profile for user who sign up with social.
+def create_profile(backend, user, *args, **kwargs):
+
+    Profile.objects.get_or_create(user=user)
--- social_website/settings.py
+++ social_website/settings.py
@@ -160,3 +160,18 @@ SOCIAL_AUTH_FACEBOOK_SCOPE = ["email"]
 
 SOCIAL_AUTH_TWITTER_KEY = ""
 SOCIAL_AUTH_TWITTER_SECRET = ""
+
+# This will create account for user who sign in with social.
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_by_email',
+    'social_core.pipeline.user.create_user',
+    # This is added after setting a function in authentication.py to add user.
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
